6abe606f32e1954ace43bb41b2dfe6e9b13dd29c
ziho <1253416625@qq.com>
2020-06-19 15:32:28 +0800

    歌手页

---
+++ src/views/singer-list/index.vue
@@ -0,0 +1,152 @@
+<template>
+	<div>
+		<Header>
+			<div class="title" slot="middle">歌手分类</div>
+		</Header>
+		<div class="select-content">
+			<div class="select-item" :class="{'selected': select1 === index}" v-for="(item, index) in options1" :key="item.value" @click="changeSelect1(index)">{{item.label}}</div>
+		</div>
+		<div class="select-content">
+			<div class="select-item" :class="{'selected': select2 === index}" v-for="(item, index) in options2" :key="item.value" @click="changeSelect2(index)">{{item.label}}</div>
+		</div>
+		<Scroll ref="scroll" :top="'7.34rem'" :hasLazyItem="true">
+			<div class="content">
+				<ArtistItem v-for="item in artists" :key="item.id" :singer="item"></ArtistItem>
+			</div>
+		</Scroll>
+	</div>
+</template>
+
+<script>
+import {
+  getArtistList
+} from "../../api/singer"
+import {
+  SearchSinger
+} from "../../common/class"
+import Header from '../../components/header'
+import ArtistItem from "../../base/singer"
+import Scroll from "../../components/scroll"
+export default {
+  components: {
+		Scroll,
+		Header,
+    ArtistItem
+  },
+  data() {
+    return {
+      options1: [
+        {
+          label: "华语",
+          value: 7
+        }, {
+          label: "欧美",
+          value: 96
+        }, {
+          label: "日本",
+          value: 8
+        }, {
+          label: "韩国",
+          value: 16
+        }, {
+          label: "其他",
+          value: 0
+        }
+      ],
+      options2: [{
+        label: "男歌手",
+        value: 1
+      }, {
+        label: "女歌手",
+        value: 2
+      }, {
+        label: "乐队/组合",
+        value: 3
+      }],
+      select1: -1,
+      select2: -1,
+      artists: [],
+      inited: false
+    }
+  },
+  created(){
+    this.getData()
+  },
+  activated(){
+    if (this.inited){
+      this.getData()
+    }
+  },
+  updated(){
+    this.$refs.scroll.refresh()
+  },
+  watch: {
+    select1() {
+      this.getData()
+    },
+    select2() {
+      this.getData()
+    }
+  },
+  methods: {
+    async getData() {
+      const { select1, select2 } = this
+      const type = select2 === -1 ? select2 : this.options2[select2].value
+      const area = select1 === -1 ? select1 : this.options1[select1].value
+      try {
+        this.$loading.show({
+          message: "加载中...",
+          banClick: true
+        })
+        const res = await getArtistList({type, area})
+        if (res.code === 200) {
+          let artists = []
+          for (let i = 0, l = res.artists.length; i < l; i++) {
+            artists.push(new SearchSinger(res.artists[i]))
+					}
+          this.artists = artists
+        }
+      } catch (err) {
+        console.log(err)
+      }
+      this.$loading.hide()
+      this.inited = true
+    },
+    back() {
+      this.$router.back()
+    },
+    changeSelect1(index){
+      if (this.select1 !== index){
+        this.select1 = index
+      }
+    },
+    changeSelect2(index){
+      if (this.select2 !== index){
+        this.select2 = index
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title {
+	width: calc(100vw - 200px);
+	line-height: 100px;
+	text-align: center;
+}
+.select-content{
+	display: flex;
+	line-height: 60px;
+}
+.select-item{
+	padding: 0 25px;
+	color: var(--desc-color);
+}
+.selected{
+	color: var(--color-red) ;
+}
+.content{
+	padding: 0 20px;
+}
+</style>
---
+++ src/views/singer-list/singer-detail/index.vue
@@ -0,0 +1,153 @@
+<template>
+	<div class="singer-detail">
+		<Header :color="'var(--color-white)'">
+			<div v-if="singer" slot="middle" class="title">{{singer.name}}</div>
+		</Header>
+		<div class="singer-img" v-if="singer"  :style="{background: `url(${singer.picUrl}) 0 0 / cover`, transform: `scale(${scale})`}">
+			<div class="filter-mask"></div>
+		</div>
+		<Scroll ref="scroll" :probeType="3" @scroll="scroll" @scrollEnd="scroll">
+			<div class="util-div"></div>
+			<div class="content">
+				<PlayAllItem :songs="songs"></PlayAllItem>
+				<SongItem	ongItem v-for="(item, index) in songs" :activeId="musicInfo.id" :key="item.id" :index="index" :song="item" @play="handlePlay"></SongItem>
+			</div>
+		</Scroll>
+	</div>
+</template>
+
+<script>
+import { mapState } from "vuex"
+
+import {
+  Song
+} from "../../../common/class"
+import {
+  getSingerSongs
+} from "../../../api/singer"
+import {
+  throttle
+} from "../../../utils"
+import Scroll from "../../../components/scroll"
+import SongItem from "../../../base/song"
+import Header from '../../../components/header'
+import PlayAllItem from '../../../base/playall-item'
+export default {
+  components: {
+		Scroll,
+		Header,
+		SongItem,
+		PlayAllItem
+  },
+  data() {
+    return {
+      singer: null,
+      songs: [],
+      scale: 1,
+      inited: false
+    }
+  },
+  computed: {
+    ...mapState(["musicInfo"])
+  },
+  created() {
+    this.getData()
+  },
+  activated() {
+    if (this.inited) {
+      this.getData()
+    }
+  },
+  mounted() {
+    this.changeScale = throttle((args) => {
+      this.scale = args && args[0] || this.scale
+    }, 50)
+  },
+  methods: {
+    async getData() {
+      const { id } = this.$route.params
+      try {
+        this.$loading.show({
+          message: "加载中...",
+          banClick: true
+        })
+        const songsRes = await getSingerSongs({id})
+        let songs = []
+				this.singer = songsRes.artist
+				for (let i = 0, l = songsRes.hotSongs.length; i < l; i++) {
+					songs.push(new Song(songsRes.hotSongs[i]))
+				}
+				this.songs = songs
+				this.$refs.scroll.refresh()
+      } catch (err) {
+        console.log(err)
+      }
+      this.$loading.hide()
+      this.inited = true
+    },
+    back() {
+      this.$router.back()
+    },
+    scroll(params){
+			if(!params || params.y <= 0){
+				this.changeScale(1)
+			} else {
+        this.changeScale(1 + params.y / 300)
+      }
+    },
+    handlePlay(song, index) {
+      this.$store.dispatch("playMusic", song.id)
+      this.$store.dispatch("setCurrentIndex", index)
+      this.$store.dispatch("setPlayList", this.songs)
+    },
+    playAll() {
+      this.handlePlay(this.songs[0], 0)
+    }
+  }
+}
+</script>
+
+<style  scoped>
+Header {
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: 10;
+	display: flex;
+	width: 100%;
+	height: var(--header-height);
+	font-size: var(--title-size);
+	color: var(--color-white);
+}
+.title {
+	width: 550px;
+	line-height: 100px;
+	text-align: center;
+}
+.singer-img {
+	position: relative;
+	z-index: -100;
+	height: 0;
+	padding-top: 75%;
+	width: 100%;
+	transform-origin: center top;
+	transition: transform .1s;
+  will-change: transform;
+}
+.filter-mask {
+	position: absolute;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	background: rgba(0, 0, 0, .2);
+}
+.util-div {
+	width: 100%;
+	height: 1px;
+	padding-top: 13.5rem;
+}
+.content{
+	background: var(--color-white);
+}
+</style>
